d7caca86c82effdf40aca4d9f832ddaad6150b9a
DaveArango <jhonatandavidvivasarango@gmail.com>
2026-09-21 19:17:53 -0500

    Enums Rol y EstadoUsuario.

---
+++ handgelus-back/src/main/java/com/uniquindio/handgelus_back/enums/EstadoUsuario.java
@@ -0,0 +1,7 @@
+package com.uniquindio.handgelus_back.enums;
+
+public enum EstadoUsuario {
+    ACTIVO,
+    INACTIVO,
+    BLOQUEADO
+}
---
+++ handgelus-back/src/main/java/com/uniquindio/handgelus_back/enums/Rol.java
@@ -0,0 +1,8 @@
+package com.uniquindio.handgelus_back.enums;
+
+public enum Rol {
+    CLIENTE,
+    ASISTENTE,
+    TATUADOR_ARTISTA,
+    ADMINISTRADOR
+}
