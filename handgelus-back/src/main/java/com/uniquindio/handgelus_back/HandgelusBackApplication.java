9d0b15c7f5fa4f2d2797d90569fd35bf54a839c1
DaveArango <jhonatandavidvivasarango@gmail.com>
2026-09-21 19:16:03 -0500

    Arquetipo de arquitectura MVC del proyecto.

---
+++ handgelus-back/src/main/java/com/uniquindio/handgelus_back/HandgelusBackApplication.java
@@ -0,0 +1,13 @@
+package com.uniquindio.handgelus_back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HandgelusBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HandgelusBackApplication.class, args);
+	}
+
+}
---
+++ handgelus-back/src/test/java/com/uniquindio/handgelus_back/HandgelusBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uniquindio.handgelus_back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HandgelusBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
